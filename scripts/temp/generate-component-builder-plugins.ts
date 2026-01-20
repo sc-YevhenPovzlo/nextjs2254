ac95b9559fa56b4268e8bb1394b93bbb028fa5c3
Yevhen Povzlo <59564875+sc-YevhenPovzlo@users.noreply.github.com>
2026-01-20 14:52:36 +0200

    Add files via upload

---
+++ scripts/temp/generate-component-builder-plugins.ts
@@ -0,0 +1,4 @@
+export { componentBuilderPlugin } from 'scripts/generate-component-builder/plugins/component-builder';
+export { componentsPlugin } from 'scripts/generate-component-builder/plugins/components';
+export { feaasPlugin } from 'scripts/generate-component-builder/plugins/feaas';
+export { packagesPlugin } from 'scripts/generate-component-builder/plugins/packages';
