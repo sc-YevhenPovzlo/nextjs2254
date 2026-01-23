f99f45a30cd52a6211aef91808b9fdc4d6a446b6
povzlo <povzlo.eugene@gmail.com>
2026-01-23 14:12:20 +0200

    mjnjm

---
+++ src/lib/middleware/plugins/custom2.ts
@@ -0,0 +1,17 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { MiddlewarePlugin } from '..';
+
+class CustomPlugin implements MiddlewarePlugin {
+  order = -4;
+
+  async exec(req: NextRequest): Promise<NextResponse> {
+    const url = req.nextUrl.clone();
+    const locale = url.locale;
+    // Check if the pathname is not already in lowercase
+    if (locale !== locale.toLowerCase()) {
+      // Redirect to the lowercase version of the URL using a 301 permanent redirect
+      return NextResponse.redirect(locale.toLowerCase()+'/'+ url.pathname);
+    }
+  }
+}
+export const customPlugin = new CustomPlugin();
