57f81a05955dc11a192a36c29749c0069038c67b
povzlo <povzlo.eugene@gmail.com>
2026-01-23 10:43:43 +0200

    fgdg

--- src/lib/middleware/plugins/custom.ts
+++ src/lib/middleware/plugins/custom.ts
@@ -5,6 +5,9 @@ class CustomPlugin implements MiddlewarePlugin {
   order = 2;
 
   async exec(req: NextRequest): Promise<NextResponse> {
+    console.log('req.nextUrl.locale: ' + req.nextUrl.locale);
+    console.log('req.nextUrl.pathname: ' + req.nextUrl.pathname);
+    console.log('req.nextUrl.origin: ' + req.nextUrl.origin);
     console.log('req.nextUrl: ' + req.nextUrl);
     console.log('req.url: ' + req.url);
     return NextResponse.next();
