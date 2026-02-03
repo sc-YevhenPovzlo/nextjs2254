3ecb899633cd8cb250bdd6c8c51fc4fd7bdaacc3
povzlo <povzlo.eugene@gmail.com>
2026-02-03 17:43:35 +0200

    ddddddddddddd

--- src/lib/middleware/plugins/redirectsCustom.ts
+++ src/lib/middleware/plugins/redirectsCustom.ts
@@ -21,7 +21,7 @@ class RedirectsCustomPlugin implements MiddlewarePlugin {
       excludeRoute: () => false,
       // This function determines if the middleware should be turned off.
       // By default it is disabled while in development mode.
-      disabled: () => process.env.NODE_ENV === 'development',
+      disabled: () => false,
       // Site resolver implementation
       siteResolver,
     });
