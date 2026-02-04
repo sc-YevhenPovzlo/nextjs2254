f648b7e0ef30c3643159e9eaae8c358da378b21f
povzlo <povzlo.eugene@gmail.com>
2026-02-04 14:07:38 +0200

    12

--- src/lib/middleware/plugins/custom.ts
+++ src/lib/middleware/plugins/custom.ts
@@ -1,11 +1,10 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 import { MiddlewarePlugin } from '..';
 
 class CustomPlugin implements MiddlewarePlugin {
-  order = -4;
+  order = 4;
 
-  async exec(req: NextRequest, res?: NextResponse): Promise<NextResponse> {
-    console.log('Run custom middleware:' + req.url + ' ' + res?.url);
+  async exec(): Promise<NextResponse> {
     return NextResponse.next();
   }
 }
