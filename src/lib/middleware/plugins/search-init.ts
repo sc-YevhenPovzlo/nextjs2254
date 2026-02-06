b29ac3800906da11bf6a8ea0058b3e612ae5268a
povzlo <povzlo.eugene@gmail.com>
2026-02-06 13:27:42 +0200

    sdf2

--- src/lib/middleware/plugins/search-init.ts
+++ src/lib/middleware/plugins/search-init.ts
@@ -15,17 +15,18 @@ class SearchInitPlugin implements MiddlewarePlugin {
       siteName: config.sitecoreSiteName,
       enableServerCookie: false,
       sitecoreEdgeUrl: config.sitecoreEdgeUrl,
-    })
-      //.addEvents() // Initialize the search package
-      //.addSearch()
-      .initialize(); // Run the initialization logic and set cookies
-    if (language != 'default') {
-      response.headers.set('rssfeed_language', language);
-    } else {
-      response.headers.set('rssfeed_language', '');
-    }
+    });
+    //.addEvents() // Initialize the search package
+    //.addSearch()
+    //   .initialize(); // Run the initialization logic and set cookies
+    // if (language != 'default') {
+    //   response.headers.set('rssfeed_language', language);
+    // } else {
+    //   response.headers.set('rssfeed_language', '');
+    // }
     return response;
   }
 }
 
 export const searchInitPlugin = new SearchInitPlugin();
+
